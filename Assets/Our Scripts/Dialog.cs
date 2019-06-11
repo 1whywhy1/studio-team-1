3f27ac5c12fd2cfd2ae1c363653b0c2fb9af1670
Ivan K <48420289+1whywhy1@users.noreply.github.com>
2019-06-11 10:26:23 +1000

    Added sound for Continue Button
    Basic dialog system with sound

--- Assets/Our Scripts/Dialog.cs
+++ Assets/Our Scripts/Dialog.cs
@@ -9,21 +9,31 @@ public class Dialog : MonoBehaviour
     public string[] sentenses;
     private int index;
     public float typingSpeed = 1f;
+    public AudioSource source;
+
+    public GameObject continueButton;
 
     // Start is called before the first frame update
     void Start()
     {
+        source = GetComponent<AudioSource>();
         StartCoroutine(CoType());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(textDisplay.text == sentenses[index])
+        {
+            continueButton.SetActive(true);
+        }
     }
 
     public void NextSentence()
     {
+        source.Play();
+        continueButton.SetActive(false);
+
         if (index<sentenses.Length - 1)
         {
             index++;
