620576b7939b6c897d503e6e24481eda8ff56a79
Ivan K <48420289+1whywhy1@users.noreply.github.com>
2019-06-11 10:02:01 +1000

    Started work on NPC Dialog System
    Created script and TextMesh Pro for dialog system

---
+++ Assets/Our Scripts/Dialog.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialog : MonoBehaviour
+{
+    public TextMeshProUGUI textDisplay;
+    public string[] sentenses;
+    private int index;
+    public float typingSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(CoType());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NextSentence()
+    {
+        if (index<sentenses.Length - 1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(CoType());
+        }
+        else
+        {
+            textDisplay.text = "";
+        }
+
+    }
+
+    IEnumerator CoType ()
+    {
+        foreach(char letter in sentenses[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+}
