d90cda692a1d1f276bf4b211536ee47a636b1966
Ivan Karyakin <48420289+1whywhy1@users.noreply.github.com>
2019-06-14 12:32:10 +1000

    Worked on PickUp Script

--- Assets/Our Scripts/PickUp.cs
+++ Assets/Our Scripts/PickUp.cs
@@ -1,13 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PickUp : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    RaycastHit hit;
+    private Camera playerCam;
+    private bool inPoV;
+    private bool inRange;
+    int layerMask = 8;
+    private float pickUpRange;
+
+    public TextMeshProUGUI useText;
+
+    private void Start()
     {
-        
+        playerCam = Camera.main;
+    }
+
+    private void Update()
+    {
+        InSight();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -17,16 +31,40 @@ public class PickUp : MonoBehaviour
             return;
         }
 
-        PickUpItem(other.transform);
+        inRange = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag != "Player")
+        {
+            return;
+        }
+
+        inRange = false;
+    }
+
+    private void InSight()
+    {
+        if (inRange)
+        {
+            Ray playerAim = playerCam.GetComponent<Camera>().ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+            if (Physics.Raycast(playerAim, out hit, pickUpRange, layerMask))
+            {
+                if (hit.collider.tag == "pickUps")
+                {
+                    useText.enabled = true;
+                }
+                else
+                {
+                    useText.enabled = false;
+                }
+            }
+        }
     }
 
     public virtual void OnPickup(Transform item)
     {
         //emty
-    }
-
-    void PickUpItem(Transform item)
-    {
-
     }
 }
---
+++ Assets/Our Scripts/PickUpPrototype.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PickUpPrototype : MonoBehaviour
+{
+    public TextMeshProUGUI pickUpText;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            pickUpText.enabled = true;
+            if(Input.GetButtonDown("E"))
+            {
+                Destroy(this);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            pickUpText.enabled = false;
+        }
+    }
+}
