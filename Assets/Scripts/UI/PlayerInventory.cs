3d98dfe4fb26b332d96bafa973b1d945dba38589
Tony G <ssj.tony89@gmail.com>
2019-07-01 13:25:28 +1000

    refactored character manager script + added temp UI

---
+++ Assets/Scripts/UI/PlayerInventory.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+	public GameObject inventoryScreen;
+	private bool inventoryShowing = false;
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.I))
+		{
+			// Show/hide menu
+			if (inventoryShowing)
+			{
+				inventoryScreen.SetActive(true);
+			}
+			else
+			{
+				inventoryScreen.SetActive(false);
+			}
+		}
+	}
+}
