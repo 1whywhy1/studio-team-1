cb4d3d7152009f96e1a8323f0ab10cfe43afa44a
Ivan <48420289+1whywhy1@users.noreply.github.com>
2019-07-04 17:54:59 +1000

    Added BG music script and a font
    In game BG music playing after 5 seconds delay. maybe will be switched by a trigger in the future.
    Added Minecraft font for instructions display.
    Added inventory to Prototype A Scene

---
+++ Assets/Scripts/Environment/BGMusicController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMusicController : MonoBehaviour
+{
+
+    private AudioSource musicPlayer;
+    public AudioClip inGameBG;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicPlayer = this.GetComponent<AudioSource>();
+        StartCoroutine(CoWait());
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    IEnumerator CoWait()
+    {
+     
+        yield return new WaitForSeconds(5);
+        musicPlayer.Play();
+    }
+}
