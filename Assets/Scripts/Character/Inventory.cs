2ca0b041b3be31bb3f01319b45952a26289f6a51
Tony G <ssj.tony89@gmail.com>
2019-07-06 01:11:07 +1000

    a part of the last commit

---
+++ Assets/Scripts/Character/Inventory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Inventory : MonoBehaviour, IInventory
+{
+	// Get max slots amount and fresh dictionary
+	[Header("Interface Requirements")] public int maxSlots = 12;
+	public Dictionary<string, int> items = new Dictionary<string, int>();
+
+	// Implement above as per interface requirement
+	public int MaxSlots => maxSlots;
+	public Dictionary<string, int> InventoryItems => items;
+
+	public enum ItemType { Food, Part, Rag, Ration }
+	public TMP_Text[] pickupNotifications;
+
+	[SerializeField] private int currentCount = 0;
+
+	// add items + check in case of avoid overflow of items
+	public void AddItem(string itemName)
+	{
+		Debug.Log(itemName);
+		Debug.Log(InventoryItems[itemName] + ": " + currentCount);
+
+		if (!InventoryItems.ContainsKey(itemName))
+		{
+			InventoryItems.Add(itemName, 1);
+		}
+		else if (InventoryItems.TryGetValue(itemName, out var currentCount))
+		{
+			InventoryItems[itemName] = currentCount++;
+		}
+	}
+
+	// used when things go in/out of inventory
+	void RearrangeInventory() {}
+}
---
+++ Assets/Scripts/Interfaces.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IInventoryItem
+{
+	string Name { get; }
+	Sprite Sprite { get; }
+}
+
+public interface IInventory
+{
+	int MaxSlots { get; }
+	Dictionary<string, int> InventoryItems { get; }
+}
