76c584d0f1e25b1f6fd5f991edfdc0f16fabfcd8
Ivan K <48420289+1whywhy1@users.noreply.github.com>
2019-07-09 19:50:31 +1000

    Update DummyScript.cs

--- Assets/Scripts/DummyScript.cs
+++ Assets/Scripts/DummyScript.cs
@@ -4,10 +4,38 @@ using UnityEngine;
 
 public class DummyScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Vector2[][] NPCTradeRatio;
+       // Start is called before the first frame update
     void Start()
     {
-        
+        //NPCTradeRatio[0][0] = new Vector2(1, 3);
+        //NPCTradeRatio[0][1] = new Vector2(2, 3);
+        //NPCTradeRatio[0][2] = new Vector2(2, 1);
+        //NPCTradeRatio[0][3] = new Vector2(3, 1);
+        //NPCTradeRatio[0][4] = new Vector2(1, 1);
+        //NPCTradeRatio[0][5] = new Vector2(2, 3);
+
+        //NPCTradeRatio[1][0] = new Vector2(4, 1);
+        //NPCTradeRatio[1][1] = new Vector2(1, 1);
+        //NPCTradeRatio[1][2] = new Vector2(3, 5);
+        //NPCTradeRatio[1][3] = new Vector2(2, 3);
+        //NPCTradeRatio[1][4] = new Vector2(1, 2);
+        //NPCTradeRatio[1][5] = new Vector2(1, 3);
+
+        //NPCTradeRatio[2][0] = new Vector2(3, 1);
+        //NPCTradeRatio[2][1] = new Vector2(1, 1);
+        //NPCTradeRatio[2][2] = new Vector2(1, 3);
+        //NPCTradeRatio[2][3] = new Vector2(1, 2);
+        //NPCTradeRatio[2][4] = new Vector2(2, 1);
+        //NPCTradeRatio[2][5] = new Vector2(1, 1);
+
+        //NPCTradeRatio[3][0] = new Vector2(3, 1);
+        //NPCTradeRatio[3][1] = new Vector2(2, 3);
+        //NPCTradeRatio[3][2] = new Vector2(1, 3);
+        //NPCTradeRatio[3][3] = new Vector2(1, 3);
+        //NPCTradeRatio[3][4] = new Vector2(1, 1);
+        //NPCTradeRatio[3][5] = new Vector2(3, 1);
+
     }
 
     // Update is called once per frame
