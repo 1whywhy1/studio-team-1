4a3e1e00ebec9c06efc394a63b9a37356f89f7e3
Ivan Karyakin <48420289+1whywhy1@users.noreply.github.com>
2019-07-12 14:09:16 +1000

    Added CameraExtensions.cs for Rox ability

---
+++ Assets/Scripts/Environment/EvilPcInteraction.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EvilPcInteraction : MonoBehaviour
+{
+    public TextMeshProUGUI interact;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            interact.gameObject.SetActive(true);
+        }
+
+        if (Input.GetKeyDown("E"))
+        {
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            interact.gameObject.SetActive(false);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
