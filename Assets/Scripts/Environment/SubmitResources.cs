f5a4f95cba40f3729d253ceb62de49c3dbc35b84
Ivan Karyakin <48420289+1whywhy1@users.noreply.github.com>
2019-07-25 19:06:13 +1000

    worked on the script

---
+++ Assets/Scripts/Environment/SubmitResources.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SubmitResources : MonoBehaviour
+{
+    public TextMeshProUGUI[] player;            //Player inventory
+    public TextMeshProUGUI[] requirement;       //Requirement values
+    //public TextMeshProUGUI error;
+
+    public void CheckTheItems()
+    {
+        for (int i= 0; i < 4; ++i){
+            int playerInt = (int.Parse(player[i].text));
+            int requirementInt = (int.Parse(requirement[i].text));
+            if (playerInt >= requirementInt)
+            {
+                playerInt -= requirementInt;
+                player[i].text = playerInt.ToString();
+            }
+            else
+            {
+                // Display error here
+                break;
+            }
+        }
+
+    }
+
+}
