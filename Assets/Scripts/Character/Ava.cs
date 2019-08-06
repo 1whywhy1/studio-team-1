8e87d0b9b0e0f918fefc154677bc4f018e83a47f
Ricky Wu <40745547+rickywjr@users.noreply.github.com>
2019-08-06 14:42:38 +1000

    Food dispenser script added

---
+++ Assets/Scripts/Character/Ava.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ava : MonoBehaviour
+{
+   
+
+    CharacterController Ccontroller;
+
+    public bool isRange;
+
+    public Transform spawnLocation;
+
+    public GameObject[] pieces;
+
+    public GameObject foods;
+    public GameObject meds;
+    public GameObject rags;
+    public GameObject parts;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isRange = false;
+        Ccontroller = GetComponent<CharacterController>();
+
+        pieces = new GameObject[4];
+        pieces[0] = meds;
+        pieces[1] = rags;
+        pieces[2] = foods;
+        pieces[3] = parts;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E)&& isRange == true)
+        {
+            Instantiate(pieces[Random.Range(0, pieces.Length)], spawnLocation.position, spawnLocation.rotation);
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Ava Variant")
+        {
+            isRange = true;
+        }
+        else
+        {
+            isRange = false;
+        }
+    }
+
+}
