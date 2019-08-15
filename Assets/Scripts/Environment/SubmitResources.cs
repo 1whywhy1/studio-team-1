0d68be902b62510620bdeb8849c2abe52c275c63
Ivan Karyakin <48420289+1whywhy1@users.noreply.github.com>
2019-08-15 20:32:40 +1000

    Updated SubmitResources.cs
    Added failure and success sounds. Implemented it into the scene

--- Assets/Scripts/Environment/SubmitResources.cs
+++ Assets/Scripts/Environment/SubmitResources.cs
@@ -7,23 +7,29 @@ public class SubmitResources : MonoBehaviour
 {
     public TextMeshProUGUI[] player;            //Player inventory
     public TextMeshProUGUI[] requirement;       //Requirement values
-    //public TextMeshProUGUI error;
+    public TextMeshProUGUI errorMessage,successMessage;        //To display an event message
+
+    public AudioClip success,failure;
+    public AudioSource audioSource;
+
 
     public void CheckTheItems()
     {
         bool hasEnough = true;
-        int[] playerInt;
-        int[] requirementInt;
+        int[] playerInt;                    //array to store player's resources in INT
+        int[] requirementInt;               //array to store required from the player resources in INT
 
         playerInt = new int[4];
         requirementInt = new int[4];
 
+        //Populate arrays
         for (int i = 0; i < 4; ++i)
         {
             playerInt[i] = (int.Parse(player[i].text));
             requirementInt[i] = (int.Parse(requirement[i].text));
         }
 
+        //Check if player has enough of each resource
         for (int i= 0; i < 4; ++i)
         {
             if (playerInt[i] < requirementInt[i])
@@ -33,19 +39,32 @@ public class SubmitResources : MonoBehaviour
             }
         }
 
+        
         if (hasEnough)
         {
+            //Takes the resources from the player
             for(int i = 0; i < 4; ++i)
             {
                 playerInt[i] -= requirementInt[i];
                 player[i].text = playerInt[i].ToString();
             }
+            //displays an success message, plays a success sound and starts a coroutine to switch the message off
+            successMessage.gameObject.SetActive(true);
+            audioSource.PlayOneShot(success);
+            StartCoroutine(CoWaitForMessage(successMessage));
         }
         else
         {
-            // Display error here
-            Debug.Log("You fool!");
+            //displays an error message, plays a failure sound and starts a coroutine to switch the message off
+            errorMessage.gameObject.SetActive(true);
+            audioSource.PlayOneShot(failure);
+            StartCoroutine(CoWaitForMessage(errorMessage));
         }
     }
 
+    IEnumerator CoWaitForMessage(TextMeshProUGUI eventMessage)
+    {
+        yield return new WaitForSeconds(3.0f);
+        eventMessage.gameObject.SetActive(false);
+    }
 }
