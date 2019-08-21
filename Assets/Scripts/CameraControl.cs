020e1e77fa4285706826bbf4051b3f66d391a8c9
Tony Etro <ssj.tony89@gmail.com>
2019-08-21 11:09:41 +1000

    added smooth camera movement

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -15,6 +15,7 @@ public class CameraControl : MonoBehaviour
 	public GameObject cam1;
 	public GameObject trueTarget;
 	public GameObject targetZoom;
+	[Range(0f, 1f)]public float cameraSmooth;
 
 	private float desiredDistance = 3f;
 	private float pitch = 0f; // controls up and down
@@ -130,7 +131,7 @@ public class CameraControl : MonoBehaviour
         // Changed to lerp to achieve a smoother followup on the character
 
         // Camera's position - Start from the player's position and go backwards a distance desiredDistance from the player.
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, trueTarget.transform.position - desiredDistance * transform.forward + Vector3.up * 1.5f, 0.2f);
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, trueTarget.transform.position - desiredDistance * transform.forward + Vector3.up * 1.5f, cameraSmooth);
         transform.position = smoothedPosition;
     }
 }
