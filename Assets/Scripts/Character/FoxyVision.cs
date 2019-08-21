6f415afc64c286d141752723b9301cf6a5f45b71
Ivan Karyakin <48420289+1whywhy1@users.noreply.github.com>
2019-08-21 11:53:44 +1000

    Update FoxyVision.cs

--- Assets/Scripts/Character/FoxyVision.cs
+++ Assets/Scripts/Character/FoxyVision.cs
@@ -1,14 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static CameraExtensions;                          // Using static class CameraExtensions to manage Layers that are visible in the Camera View
+using static CameraExtensions;                                    // Using static class CameraExtensions to manage Layers that are visible in the Camera View
 using UnityEngine.Rendering.PostProcessing;
 
 public class FoxyVision : MonoBehaviour
 {
     public Camera cam;
-    private bool disablePower = false;                      // To restrict player from spamming E
-    public LayerMask secretLayer;                       // Select a layer mask to be toggled for the camera
+    private bool disablePower = false;                           // To restrict player from spamming E
+    public LayerMask secretLayer;                                // Select a layer mask to be toggled for the camera
     public Move rox;
     public PostProcessVolume foxEffect;
 
@@ -21,9 +21,9 @@ public class FoxyVision : MonoBehaviour
             {
                 if (Input.GetKeyDown(KeyCode.Mouse0))
                 {
-                    foxEffect.enabled = !foxEffect.enabled; // Switch on the post processing
+                    foxEffect.enabled = !foxEffect.enabled;     // Switch on the post processing
                     disablePower = true;                        // Disables the E button check for the player input
-                    cam.LayerCullingShow(secretLayer);      // Adds a layer mask to camera rendering
+                    cam.LayerCullingShow(secretLayer);          // Adds a layer mask to camera rendering
                     StartCoroutine(CoWait());
                 }
             }
@@ -32,9 +32,9 @@ public class FoxyVision : MonoBehaviour
 
     IEnumerator CoWait()
     {
-        yield return new WaitForSeconds(10);
-        cam.LayerCullingHide(secretLayer);              // Hides a layer mask from the camera
-        foxEffect.enabled = !foxEffect.enabled;         // Switch off the post processing
-        disablePower = false;                               // Enables E button check for the player input
+        yield return new WaitForSeconds(10);    
+        cam.LayerCullingHide(secretLayer);                     // Hides a layer mask from the camera
+        foxEffect.enabled = !foxEffect.enabled;                // Switch off the post processing
+        disablePower = false;                                  // Enables E button check for the player input
     }
 }
